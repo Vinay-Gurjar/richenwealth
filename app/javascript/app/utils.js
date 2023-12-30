7f99d38190dbf826aa42856f65133ffb6acdb719
Vijay Gurjar <vijay.kumar@jarvis.consulting>
2023-12-31 02:12:03 +0530

    '(upload users) (login) (hourly report) (mark attendance) (agent all day report) (minimum calls) (tl wise report) are fixed Home page Design Fixed'

--- app/javascript/app/utils.js
+++ app/javascript/app/utils.js
@@ -1,3 +1,15 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import {toast} from 'react-toastify';
+import React from "react";
+import {faInfoCircle} from "@fortawesome/free-solid-svg-icons";
+
+export const attendancePath = '/'
+export const hourlyReport = '/hourly_report'
+export const uploadFiles = '/upload_files'
+export const minimumCallConnected = '/minimum_calls_connected'
+export const attendanceDashboard = '/attendance_dashboard'
 
 export const isValuePresent = (value) => {
     return (
@@ -19,8 +31,57 @@ export const findValueFromArray = (listArray, findValue) => {
    return  listArray.find(value => value.id === findValue)
 }
 
-export const attendancePath = '/'
-export const hourlyReport = '/hourly_report'
-export const uploadFiles = '/upload_files'
-export const minimumCallConnected = '/minimum_calls_connected'
-export const attendanceDashboard = '/attendance_dashboard'
+
+
+export const disabledSaveProgressButton =
+    <Tooltip title="In view mode, data saving is not available.">
+        <Button>Save Progress
+            <FontAwesomeIcon className='save-progress-info' icon={faInfoCircle} style={{color: "#3f96fd"}}/>
+        </Button>
+    </Tooltip>
+
+
+export const showSuccessToast = (massage) => {
+    const isContainerPresent = document.querySelector('.Toastify__toast--success');
+    if (isContainerPresent) {
+        isContainerPresent.remove();
+    }
+
+    toast.success(massage, {
+        position: 'bottom-right',
+        autoClose: 3000, // milliseconds
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+    });
+
+}
+
+export const showErrorToast = (massage) => {
+    const isContainerPresent = document.querySelector('.Toastify__toast-theme--light.Toastify__toast--error.Toastify__toast--close-on-click');
+console.log(massage)
+    if (!isContainerPresent) {
+        toast.error(massage, {
+            position: 'top-right',
+            autoClose: 3000, // milliseconds
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+        });
+    }
+}
+
+export const showNotification = () => {
+    toast.info('In view mode, data is not saving.', {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    });
+}
